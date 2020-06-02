3e7119be04d3100d309b5b8a0034c82d3c16be36
Ashish Gupta <hackerashish25@gmail.com>
2020-06-02 18:55:49 +0530

    prob1 updated

---
+++ problem 1.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+vector<int> func(int arr[],int tar,int sz){
+        vector<int> v;
+        int dp[tar+1]={0};
+        for(int i=0;i<sz;i++){
+            dp[arr[i]]=i+1;
+        }
+        for(int i=1;i<=sz;i++){
+            int fin = tar-arr[i-1];
+            if(dp[fin]!=0&&dp[fin]!=i){
+                v.push_back(i-1);
+                v.push_back(dp[fin]-1);
+                return v;
+            }
+        }
+
+        return v;
+
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;cin>>n;////size of array
+    int tar;cin>>tar;
+    int arr[n];
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+
+    vector<int> v = func(arr,tar,n);
+
+    if(v.size()==0)//not possible
+     cout<<"not possible";
+    else{
+        for(int i=0;i<v.size();i++)
+            cout<<v[i]<<" ";
+    }
+}
